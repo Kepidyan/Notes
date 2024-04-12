77b475fb383316c42dc4b87593e102cce51d2961
Areg <kepidyan@MacBook-Air-Areg.local>
2024-04-13 03:12:00 +0400

    add new note function,delate node dont working

--- script/index.js
+++ script/index.js
@@ -1,12 +1,21 @@
-const newNotes = document.getElementById('btn')
+const newNotes = document.getElementById('creatNewNotes');
+let num = 1
+newNotes.addEventListener('click', () => {
+    const notes = document.getElementById('notes');
+    const newNote = document.createElement('div');
+    newNote.classList.add('newNote');
+    newNote.setAttribute('id','newNote')
+    notes.appendChild(newNote);
+   newNote.innerHTML += `
+   <h1>Note ${num}</h1>
+   <div contenteditable="true" class="note-content"  placeholder="Введите вашу заметку"></div>
+   
+   `
+   num++
+});
 
-newNotes.addEventListener('click',()=>{
-    const notes = document.getElementById('notes')
-    
-    notes.innerHTML += `
-        <div class ="creatNewNotes">
-        
-        </div>
-    `
+const note = document.getElementById('newNote');
+note.addEventListener('dblclick',() => {
+    alert('dealet note?');
     console.log('barev');
 })
