db6bb160e34497d5dad3b66e3d28f576d5d25f4c
Areg <kepidyan@MacBook-Air-Areg.local>
2024-04-13 04:36:36 +0400

    del note 50%

--- script/index.js
+++ script/index.js
@@ -11,11 +11,37 @@ newNotes.addEventListener('click', () => {
    <div contenteditable="true" class="note-content"  placeholder="Введите вашу заметку"></div>
    
    `
+   const note = document.getElementById('newNote');
+    note.addEventListener('dblclick',(e) => {
+    let del = confirm('dealet note?');
+    if(del){
+        deleteNote(id, element);
+    }
+})
    num++
 });
+function deleteNote(id, element) {
+    const notes = getNotes().filter((note)=>note.id != id)
+    saveNote(notes)
+    appEl.removeChild(element)
+}
+function getNotes() {
+    return JSON.parse(localStorage.getItem("note-app") || "[]");
+}
 
-const note = document.getElementById('newNote');
-note.addEventListener('dblclick',() => {
-    alert('dealet note?');
-    console.log('barev');
-})
+function saveNote(notes) {
+    localStorage.setItem("note-app", JSON.stringify(notes));
+}
+function addNote() {
+    const notes = getNotes();
+    const noteObj = {
+      id: Math.floor(Math.random() * 100000),
+      content: "",
+    };
+    const noteEl = createNoteEl(noteObj.id, noteObj.content);
+    appEl.insertBefore(noteEl, btnEl);
+  
+    notes.push(noteObj);
+  
+    saveNote(notes);
+  }
