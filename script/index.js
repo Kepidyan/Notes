4d0548d2d3c5647d22ae13fd1bc0201560ea91af
Areg <kepidyan@MacBook-Air-Areg.local>
2024-04-18 20:09:56 +0400

    finish

--- script/index.js
+++ script/index.js
@@ -1,47 +1,26 @@
-const newNotes = document.getElementById('creatNewNotes');
+const newNotesCreat = document.getElementById('creatNewNotes');
 let num = 1
-newNotes.addEventListener('click', () => {
+newNotesCreat.addEventListener('click', () => {
+
     const notes = document.getElementById('notes');
     const newNote = document.createElement('div');
     newNote.classList.add('newNote');
-    newNote.setAttribute('id','newNote')
+    newNote.setAttribute('id',`newNote${num}`);
     notes.appendChild(newNote);
    newNote.innerHTML += `
-   <h1>Note ${num}</h1>
+   <h1>Note</h1>
    <div contenteditable="true" class="note-content"  placeholder="Введите вашу заметку"></div>
-   
    `
-   const note = document.getElementById('newNote');
+   const note = document.getElementById(`newNote${num}`);
     note.addEventListener('dblclick',(e) => {
     let del = confirm('dealet note?');
     if(del){
-        deleteNote(id, element);
+        e.target.parentNode.removeChild(e.target); 
     }
 })
+
    num++
 });
-function deleteNote(id, element) {
-    const notes = getNotes().filter((note)=>note.id != id)
-    saveNote(notes)
-    appEl.removeChild(element)
-}
-function getNotes() {
-    return JSON.parse(localStorage.getItem("note-app") || "[]");
-}
 
-function saveNote(notes) {
-    localStorage.setItem("note-app", JSON.stringify(notes));
-}
-function addNote() {
-    const notes = getNotes();
-    const noteObj = {
-      id: Math.floor(Math.random() * 100000),
-      content: "",
-    };
-    const noteEl = createNoteEl(noteObj.id, noteObj.content);
-    appEl.insertBefore(noteEl, btnEl);
-  
-    notes.push(noteObj);
-  
-    saveNote(notes);
-  }
+
+
